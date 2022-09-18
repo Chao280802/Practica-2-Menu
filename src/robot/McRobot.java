c25b3a3787a24369f4dfcde378800729a0ce8516
Chao280802 <romerocruzfernando@gmail.com>
2022-09-17 20:16:31 -0500

    Traduccion a español pal Roger

--- src/robot/McRobot.java
+++ src/robot/McRobot.java
@@ -2,9 +2,9 @@ package robot;
 import robot.states.*;
 
 public class McRobot {
-    private State activeMode;
-    private State suspendMode;
-    private State actualState;
+    private Estado activeMode;
+    private Estado suspendMode;
+    private Estado actualState;
 
     public static void main(String[] args) {
 
--- src/robot/states/Estado.java
+++ src/robot/states/Estado.java
@@ -2,7 +2,7 @@ package robot.states;
 
 import robot.states.menus.*;
 
-public interface State {
+public interface Estado {
     void walkToClientsTable();
     MenuItem takeClientOrder(Menu menus);
     void cook(MenuItem item);
