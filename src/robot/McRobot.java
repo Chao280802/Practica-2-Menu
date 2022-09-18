efe06b2515b3cd0b1b470d5c1484ea9f5c4239fe
Chao280802 <romerocruzfernando@gmail.com>
2022-09-17 20:37:40 -0500

    Traduccion 3 el regreso

--- src/robot/McRobot.java
+++ src/robot/McRobot.java
@@ -2,11 +2,7 @@ package robot;
 import robot.estados.*;
 
 public class McRobot {
-    private Estado activeMode;
-    private Estado suspendMode;
-    private Estado actualState;
-
-    public static void main(String[] args) {
-
-    }
+    private Estado modoActivo;
+    private Estado modoSuspendido;
+    private Estado estadoActual;
 }
